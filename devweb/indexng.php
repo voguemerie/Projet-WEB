b5d2b7a8def1bab847a88bb3b1c90eec189b239f
Allanow <95440096+Allanow@users.noreply.github.com>
2023-01-02 00:56:28 +0100

    Add files via upload

--- devweb/indexng.php
+++ devweb/indexng.php
@@ -1,48 +1,42 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
-session_start();
-$_SESSION["log"] ??= false;
+    header("Access-Control-Allow-Origin: *");
+    session_start();
+    $_SESSION["log"] ??= false;
 
 
-if(!$_SESSION["log"]){
-    header('Location:login.php');  //redirection sur le login lorsque l'on arrive sur la page
-    
-    exit;
-}
-                        
+    if(!$_SESSION["log"]){
+        header('Location:login.php');  //redirection sur le login lorsque l'on arrive sur la page
+        
+        exit;
+    }      
 ?>
-
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mes Musées</title>
     <!-- <script src="index_burger.js"></script> -->
+    <link rel="stylesheet" href="style_indexng.css">
     <script src="meteo.js"></script>
-     <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
     <!-- icone burger --> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAjGU6qqUqh4U4BOgbd2yIgyKHTkNw4YTc&libraries=places"></script>
     <script src="http://openlayers.org/api/OpenLayers.js"></script>
 
     <!-- <script type=text/javascript src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
-     <!-- icone utilisateur--> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <!-- icone utilisateur--> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <!-- icone serach --> <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     
 
 </head>
-
-
 <body>
-    
     <header>
         <nav>
             <ul id="nav-bar">
-                <li><a href="">Accès commentaires</a></li>
+                <li><a href="#commentaire">Accès commentaires</a></li>
                 <li><a href="">Les musées</a></li>
                 <li><a href="">Nous contacter</a></li>
                 <div id="floating-panel">
@@ -56,43 +50,44 @@ if(!$_SESSION["log"]){
                 </div>
                 <input type="search" name="search-bar" id="search-bar" placeholder="Search...">
                 <span id="search-icon" class="material-symbols-outlined">search</span>
-                <li><a href="login.php">Connexion</a></li>
-                
+                <li><a href="login.php">Déconnexion</a></li>
             </ul>
-            
         </nav>
     </header>
 
-    <p name="mess_log" id="mess_log">Bonjour, <!--<?php $_GET["login"] ?> --> vous êtes connecté</p>   
-
-    <!-- <div id="map" style="width: 1000px; height: 800px;" ></div> -->
-    <!-- changement de taille pour map à faire ici -->
-    <div id="container">
-      <div id="map" style="width: 1000px; height: 600px;" ></div>
-      <div id="sidebar"></div>
+    <p name="mess_log" id="mess_log">Bonjour, bienvenue sur la carte des musées de Paris !</p>   
+    <!----------------------- METEO ------------------------>
+    <div class="weatherCard">
+        <div class="currentTemp">
+            <span class="temp" id="zone_meteo">°C</span>
+            <span class="location">Météo de <br> Paris</span>
+        </div>
     </div>
-    
-    <p id="zone_meteo">toto</p>
-    
+    <!------------------------------------------------------>
     <div class="itineraire">
-        <div id="pac-container">
-            <input id="pac-input" type="text" placeholder="Entrer l'adresse" />
+        <div id="pac-container">    
+            <input id="pac-input" type="text" placeholder="Adresse de départ" />
         </div>
         <select id="list1"></select>
         <button id= entrer>Valider</button>
     </div>
-    
+    <div id="container">
+      <div id="map" style="width: 1000px; height: 600px;" ></div>
+      <div id="sidebar"></div>
+    </div>
 
-    <div class="commentaire">
-        <textarea  name="comment_area" id="comment_area" cols="30" rows="10" placeholder="taper votre commentaire ici"></textarea>
-        <button  id="post_comment">commenter</button>
+    <div id="commentaire" class="commentaire">
+        <h1>Envoyez nous vos commentaires !</h1>
+        <p> Votre avis nous intéresse, racontez-nous comment s'est passé votre visite :)</p>
+        <textarea name="comment_area" id="comment_area" cols="30" rows="10" placeholder="taper votre commentaire ici"></textarea>
+        <button id="post_comment">commenter</button>
     </div>
     
 
     <div id="steps" style="display: none;"></div>
 
     
-</body>
+</body> 
 
 <script>
      
@@ -101,30 +96,30 @@ if(!$_SESSION["log"]){
                 var ajaxURL = "addcomment.php";
                 var data = {'content':$("#comment_area").val(), 'user':1, 'date':Date.now()}
                 $.post(ajaxURL, data, function(res){});
+                $("#comment_area").val('');
             });
         });
-        // function initMap() {
+    // function initMap() {
     const directionsRenderer = new google.maps.DirectionsRenderer();
     const directionsService = new google.maps.DirectionsService();
     const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 48.863753, lng: 2.336715},
-    zoom: 12,
-  });
-directionsRenderer.setPanel(document.getElementById("sidebar"));
-directionsRenderer.setMap(map);
-  calculateAndDisplayRoute(directionsService, directionsRenderer);
-  document.getElementById("mode").addEventListener("change", () => {
+        center: {lat: 48.863753, lng: 2.336715},
+        zoom: 12,
+    });
+    directionsRenderer.setPanel(document.getElementById("sidebar"));
+    directionsRenderer.setMap(map);
     calculateAndDisplayRoute(directionsService, directionsRenderer);
-  });
-
-// }
+    document.getElementById("mode").addEventListener("change", () => {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    });
+    // }
         
-  const icon = {
-    url: 'icone.png', // url
-    scaledSize: new google.maps.Size(20, 25), // scaled size
-    origin: new google.maps.Point(0,0), // origin
-    anchor: new google.maps.Point(0, 0) // anchor
-};
+    const icon = {
+        url: 'icone.png', // url
+        scaledSize: new google.maps.Size(20, 25), // scaled size
+        origin: new google.maps.Point(0,0), // origin
+        anchor: new google.maps.Point(0, 0) // anchor
+    };
 
     $.ajax({
         method: 'GET',
@@ -146,7 +141,7 @@ directionsRenderer.setMap(map);
                 icon:icon
                 });
                 
-              
+
                 var select= document.getElementById ("list1");
                         var newOption = new Option (data['records'][i]['fields']["nom_officiel_du_musee"]);
                         select.options.add (newOption);
@@ -166,31 +161,20 @@ directionsRenderer.setMap(map);
                     currentInfoWindow = infowindow;
                                     });
                 })(marker, i);
-        
-    }
-                        
-                }        
+            }
+        }        
     });
-    
 
-    // $user_pop = document.getElementById("user_pop-up");
-    // function openForm() {                                               //Fonction pour faire apparaitre le formulaire user_pop-up  
-    //     document.getElementById("user_pop-up").style.display= "block";
-    //     document.getElementById("user_icon").style.display= "none";
-
-    // }
-    // function closeForm() {                                              //Fonction pour faire disparaitre le formulaire user_pop-up
-    //     $user_pop.style.display= "none";
-    //     document.getElementById("user_icon").style.display= "block";
-    // }
-
-
-
-
-
-
-
-
+    //////////////////////////////////// METEO ////////////////////////////////////////////////////////////////////
+    $.ajax({
+        type: "GET",
+        url: "https://api.openweathermap.org/data/2.5/weather?lat=48.853&lon=2.349&appid=cc496e03e4215064006f974df45c603a", 
+        success: function(retour){
+            console.log(retour.main.temp -273.15);
+            $("#zone_meteo").html(Math.floor(retour.main.temp -273.15));
+        }
+    });
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     const input = document.getElementById("pac-input");
     const options = {
@@ -209,7 +193,7 @@ directionsRenderer.setMap(map);
     autocomplete.bindTo("pac-input", map);
     const onChangeHandler = function () {
     calculateAndDisplayRoute(directionsService, directionsRenderer);
-  };
+    };
   
 
     function calculateAndDisplayRoute(directionsService, directionsRenderer) {
@@ -226,313 +210,9 @@ directionsRenderer.setMap(map);
             .then((response) => {
             directionsRenderer.setDirections(response);
             })
-            // .catch((e) => window.alert("Directions request failed due to " + status));
-        }
-        document.getElementById("entrer").addEventListener("click",onChangeHandler);
-// window.initMap = initMap;
+    }
+    document.getElementById("entrer").addEventListener("click",onChangeHandler);
+
+        
 </script>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'); /*Font Roboto*/
-    body{
-        font-family: 'Lucida Bright', sans-serif;
-        background-color: #faf0dc;
-        /* background-color: rgb(149, 138, 180); */
-        /* background-image: url("fond_form.jpg"); */
-        /* background-repeat: no-repeat;
-        background-size: cover; */
-        /* overflow: hidden; */
-        
-        /* overflow pour cacher la partie où y'a pas de background, bg-size pour la taille du bg iici recouvre tout l'écran  */
-        
-    }
-    html, body{
-        height: 100%;
-        margin: 0 0 0 0;
-        padding: 0 0 0 0;
-    }
-
-    nav{
-        background-color: #f6e8b1;
-        height: 50px;
-        /* filter: brightness(50%); */
-        margin: 0 0 0 0;
-        padding: 10px 0 0 0;
-        
-    }
-
-    #nav-bar {
-        display: flex;
-        left: 5%;
-        list-style: none;
-        margin: 0 0;
-        box-sizing: border-box;
-    }
-    #nav-bar li{
-        font-size: 16px;
-        width: max-content;
-        text-align: center;
-        margin-left: 25px;
-        float: left;
-    }
-    #nav-bar li a{
-        text-decoration: none;
-        color: inherit;
-    }
-    #nav-bar li:hover{
-        color: black;        
-        filter: brightness(90%);
-    }
-
-    #search-bar{
-        
-        display: block;
-        margin-left: 40%;
-        padding-left: 5px;
-        border: thin;
-        
-        border-radius: 6px;
-        border-color: grey;
-        font-size: 16px;
-        
-        background-color: transparent;
-        box-shadow: none;  
-              
-    }
-    #search-bar:focus{
-        color: black;
-        border: thin;
-    }
-    
-    #search-icon{
-        font-size: 20px;
-        font-variation-settings:
-        'FILL' 0,
-        'wght' 400,
-        'GRAD' 0,
-        'opsz' 48
-    }
-    .list{
-        float: left;
-    }
-    
-
-    /* form[action='logout.php'] input{
-        border: 3px solid black;
-        margin-bottom: 1rem;
-        padding: 8px 16px;
-        border-radius: 0.5rem;
-        color: black;
-        
-        width: 150px;
-        float: right;
-    }
-
-    form[action='logout.php'] input:hover{
-        filter: brightness(90%);
-    } */
-
-    #mess_log{
-        text-align: center;
-        font-size: larger;
-        color: black;
-        font-size: 25px;
-    }
-
-    /* #user_icon{
-      font-variation-settings:
-      'FILL' 200,
-      'wght' 400,
-      'GRAD' 0,
-      'opsz' 48;
-      border: none;
-      color: black;
-      float: right;   
-      width: fit-content;  
-      font-size: 40px; 
-      background-color: inherit;
-      
-    } */
-    /* #user_icon:hover{
-        filter: brightness(90%);
-    } */
-
-    /* #user_pop-up{
-        z-index: 2;
-        
-        position: absolute;
-        top: 50px;
-        right: 20px;
-        width: 10rem;
-        text-align: center;        
-        border: solid 3px black;
-        border-radius: 0.5rem;
-        box-sizing: border-box;
-        display: none;    
-        background-color: white;
-    } */
-    /* #user_pop-up input{
-        border-radius: 0.5rem;
-        padding: 2% 4%;
-        outline: none;
-        margin-bottom: 4%;
-    } */
-    /* #user_pop-up input:hover{
-        filter:brightness(90%);
-    } */
-    /* #user_pop-up input[type='submit']{
-        background-color: red;
-        color: white;
-
-    } */
-    #steps {
-        width: 280px;
-        height: 400px;
-        position: absolute;
-        right: 24px;
-        top: 24px;
-        background-color: #FFFFFF;
-        border-radius: 8px;
-        box-shadow: 0 0 16px #C0C0C0;
-        padding: 10px;
-        overflow: auto;
-        }
-
-    /* Sidenav menu */
-    /* .sidenav {
-        height: 100%;
-        width: 250px;
-        position: fixed;
-        z-index: 2;
-        top: 0;
-        left: -250px;
-        background-color: brown;
-        padding-top: 60px;
-        transition: left 0.5s ease;
-    } */
-
-    /* Sidenav menu links */
-    /* .sidenav a {
-        padding: 8px 8px 8px 32px;
-        text-decoration: none;
-        font-size: 25px;
-        color: white;
-        display: block;
-        transition: 0.3s;
-    } */
-
-    /* .sidenav a:hover {
-        color: #111;
-    } */
-
-    /* .sidenav ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    } */
-
-    /* Active class */
-    /* .sidenav.active {
-        left: 0;
-    } */
-
-    /* Close btn */
-    /* .sidenav .close {
-        position: absolute;
-        top: 0;
-        right: 25px;
-        font-size: 36px;
-    } */
-    /* #openBtn{
-        width: fit-content;
-    } */
-
-    /* #icon_burger{
-        font-variation-settings:
-        'FILL' 200
-        'wght' 400,
-        'GRAD' 0,
-        'opsz' 48;
-        font-size: 40px;
-        color: black;
-        background-color: inherit;
-    } */
-
-    /* #map{
-        float: left;
-        left: 12%;
-        z-index: 0;
-        /* visibility: hidden; 
-    }
-        */
-    
-    #container {
-  height: 100%;
-  display: flex;
-}
-
-#sidebar {
-  flex-basis: 15rem;
-  flex-grow: 1;
-  padding: 1rem;
-  max-width: 30rem;
-  height: 100%;
-  box-sizing: border-box;
-  overflow: auto;
-}
-
-#map {
-  flex-basis: 0;
-  flex-grow: 4;
-  height: 70%;
-  margin-left: 100px;
-}
-
-.commentaire{
-    margin-left: 100px;
-    z-index: 2;        
-    
-}
-.itineraire{
-    margin-left: 30px;
-    text-align: center;
-    display: flex;
-    justify-content: center;
-    
-    /* flex-wrap: wrap; */
-}
-.itineraire input,button,select{
-    border-radius: 0.5em;
-    border: 1px solid black;
-    padding-left: 12px;
-    margin-right: 20px;
-
-}
-
-.itineraire #pac-container{
-    width: fit-content;
-    
-}
-
-#floating-panel {
-  position: absolute;
-  /* top: 10px; */
-  left: 40%;
-  z-index: 5;
-  background-color: inherit;
-  padding: 5px;
-  border: 1px solid #999;
-  border-radius: 6px;
-  text-align: center;
-  font-family: "Roboto", "sans-serif";
-  line-height: 30px;
-  padding-left: 10px;
-}
-
-#floating-panel #mode{
-    background-color: inherit;
-}
-
-</style>
-
 </html>
--- devweb/login.php
+++ devweb/login.php
@@ -1,22 +1,20 @@
 <?php 
 
-   
-    
-
 ?>
 
 <!DOCTYPE html>
-<html>
+<htmllang="en">
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Mon petit musée</title>
-	<link rel="stylesheet" type="text/css" href="slide navbar style.css">
+	<!-- <link rel="stylesheet" type="text/css" href="slide navbar style.css"> -->
+	<link rel="stylesheet" href="style_login.css">
 <link href="https://fonts.googleapis.com/css2?family=Jost:wght@500&display=swap" rel="stylesheet">
 </head>
 <body>
-    
-
-</body>
-	<div class="main">  	
+	<main class="main">  	
 		<input type="checkbox" id="chk" aria-hidden="true">
 
 			<div class="signup">
@@ -24,10 +22,17 @@
 					<label for="chk" aria-hidden="true">Sign up</label>
 					<input type="text" name="txt" placeholder="User name" required="">
 					<input type="email" name="email" placeholder="Email" required="">
-					<input type="password" name="pswd" placeholder="Password" required="">
+					<input type="password" name="pass" placeholder="Password" required="">
 					<input id ="submit" type="submit" value="Sign up"><br>
 				</form>
 			</div>
+			
+			<?php
+
+				if(isset($_POST['SubmitSign'])) {
+					insertUser($_POST['txt'], $_POST['email'], $_POST['pass']);
+				}
+			?>
 			<div class="login">
 				<form action="auth.php">
 					<label for="chk" aria-hidden="true">Login</label>
@@ -38,202 +43,6 @@
 					
 				</form>
 			</div>
-	</div>
+		</main>
 </body>
-<style>
-body{
-	margin: 0;
-	padding: 0;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	min-height: 100vh;
-	font-family: 'Lucida Bright', sans-serif;
-	background-image: linear-gradient(to bottom, #0f0c29, #302b63, #24243e);
-    background-image: url("fond_form.jpg");
-    background-size: cover;
-}
-.main{
-	width: 500px;
-	height: 500px;
-	background: red;
-	overflow: hidden;
-	background: url("https://doc-08-2c-docs.googleusercontent.com/docs/securesc/68c90smiglihng9534mvqmq1946dmis5/fo0picsp1nhiucmc0l25s29respgpr4j/1631524275000/03522360960922298374/03522360960922298374/1Sx0jhdpEpnNIydS4rnN4kHSJtU1EyWka?e=view&authuser=0&nonce=gcrocepgbb17m&user=03522360960922298374&hash=tfhgbs86ka6divo3llbvp93mg4csvb38") no-repeat center/ cover;
-	border-radius: 10px;
-	box-shadow: 5px 20px 50px #000;
-}
-#chk{
-	display: none;
-}
-.signup{
-	position: relative;
-	width:100%;
-	height: 100%;
-}
-label{
-	color: #fff;
-	font-size: 2.3em;
-	justify-content: center;
-	display: flex;
-	margin: 60px;
-	font-weight: bold;
-	cursor: pointer;
-	transition: .5s ease-in-out;
-}
-input{
-	width: 60%;
-	height: 20px;
-	background: #e0dede;
-	justify-content: center;
-	display: flex;
-	margin: 20px auto;
-	padding: 10px;
-	border: none;
-	outline: none;
-	border-radius: 5px;
-    font-family: 'Lucida Bright', sans-serif;
-}
-#submit{
-	width: 60%;
-	height: 40px;
-	margin: 10px auto;
-	justify-content: center;
-	display: block;
-	color: #fff;
-	background: #dda445;
-	font-size: 1em;
-	font-weight: bold;
-	margin-top: 20px;
-	outline: none;
-	border: none;
-	border-radius: 5px;
-	transition: .2s ease-in;
-	cursor: pointer;
-    font-family: 'Lucida Bright', sans-serif;
-}
-button:hover{
-	background: #6d44b8;
-}
-.login{
-	height: 460px;
-	background: #eee;
-	border-radius: 60% / 10%;
-	transform: translateY(-180px);
-	transition: .8s ease-in-out;
-}
-.login label{
-	color: #dda445;
-	transform: scale(.6);
-}
-#forgotPass{
-	font-size: 13px;
-	text-align: right;
-	padding-right: 18%;
-	
-}
-a{
-	text-decoration: none;
-}
-a:visited{
-	color:black;
-}
-#chk:checked ~ .login{
-	transform: translateY(-500px);
-}
-#chk:checked ~ .login label{
-	transform: scale(1);	
-}
-#chk:checked ~ .signup label{
-	transform: scale(.6);
-}
-</style>
 </html>
-
-
-
-<!-- <--!<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-
-
-    <title>Document</title>
-</head>
-<body>
-    <section>
-        <h2>Connectez-vous</h2>
-        <form action="auth.php">
-            <label for="login">Login :</label> 
-            <input id ="login" type="text" name="login" placeholder="identifiant"><br>
-            <label for="password">Password :</label>
-            <input id ="password" type="password" name="password" placeholder="password"><br>
-            
-            <input class="material-symbols-outlined" id ="submit" type="submit" value="done_outline"><br>
-        </form>
-        <a href="signin.php">Vous n'êtes pas encore inscrit ?</a>
-        <p id="mess_err"></p>
-    </section>
-
-</body>
-
-<style>
-    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap'); /*Font Roboto*/
-    body{
-        font-family: 'Roboto', sans-serif;
-        background-image: url("gradienta-G084bO4wGDA-unsplash.jpg");
-    }   
-    section{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-left: -150px;
-        margin-top: -150px;
-    }
-    section h2{
-        color: grey;
-        font-size: 2rem;   
-    }
-    section a{
-        color: grey;
-        text-decoration: none;
-    }
-    input{
-        
-        padding: 8px 16px;
-        border-radius: 0.5rem;
-        border: 3px solid black;
-        margin-bottom: 1rem;
-    }
-    input[type=text], input[type=password]  {
-        color: black;
-        outline: none;
-        padding: 12px 12px;
-        width: 350px;
-    }
-    input[type=submit   ]{
-        color: black;
-        outline: none;
-        width: 90px;
-        float: right;
-
-        /* margin-left: 25%; */
-    }
-    input[type=button]:hover{
-        filter: brightness(90%);
-    }
-    .material-symbols-outlined {
-        font-variation-settings:
-        'FILL' 0,
-        'wght' 400,
-        'GRAD' 0,
-        'opsz' 48
-    }
-    #mess_err{
-        color: white;
-    }
-    
-</style>
-</html>-->
